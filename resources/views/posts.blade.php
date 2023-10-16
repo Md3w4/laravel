7f94ed133309d4535c7a81685b6044b317ad23ab
Md3w4 <mahadewamm109@gmail.com>
2023-10-16 11:09:04 +0700

    part 17

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,8 +6,16 @@
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Search.." name="search">
+                    <input type="text" class="form-control" placeholder="Search.." name="search"
+                        value="{{ request('search') }}">
                     <button class="btn btn-danger" type="submit">Search</button>
                 </div>
             </form>
@@ -23,9 +31,9 @@
                         class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
                 <p>
                     <small class="text-muted">
-                        By. <a href="/authors/{{ $posts[0]->author->username }}"
+                        By. <a href="/posts?author={{ $posts[0]->author->username }}"
                             class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
-                            href="/categories/{{ $posts[0]->category->slug }}"
+                            href="/posts?category={{ $posts[0]->category->slug }}"
                             class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                         {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
@@ -42,7 +50,8 @@
                         <div class="card">
                             <div class="position-absolute px-3 py-2 text-white"
                                 style="background-color: rgba(0, 0, 0, 0.7)"><a
-                                    href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
+                                    href="/posts?category={{ $post->category->slug }}"
+                                    class="text-white text-decoration-none">
                                     {{ $post->category->name }}</a></div>
                             <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
                                 alt="{{ $post->category->name }}">
@@ -50,7 +59,7 @@
                                 <h5 class="card-title">{{ $post->title }}</h5>
                                 <p>
                                     <small class="text-muted">
-                                        By. <a href="/authors/{{ $post->author->username }}"
+                                        By. <a href="/posts?author={{ $post->author->username }}"
                                             class="text-decoration-none">{{ $post->author->name }}</a>
                                         {{ $post->created_at->diffForHumans() }}
                                     </small>
@@ -66,4 +75,8 @@
     @else
         <p class="text-center fs-4">No post found.</p>
     @endif
+
+    <div class="d-flex justify-content-end">
+        {{ $posts->links() }}
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Models\Category;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,18 +46,16 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('author', 'category')
-    ]);
-});
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'active' => 'posts',
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
